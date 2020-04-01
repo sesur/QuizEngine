490baa5955afc434463a16f6e7aae0183eca7386
Sergiu <sergiusuru@Sergius-MacBook-Pro.local>
2020-04-01 15:42:19 +0300

    Refactoring duplications

--- QuizEngineTests/GameTest.swift
+++ QuizEngineTests/GameTest.swift
@@ -14,27 +14,26 @@ class GameTest: XCTestCase {
     let router = RouterSpy()
     var game: Game<String, String, RouterSpy>!
     
-    func test_startGame_withZeroOfTwoCorrectAnswer_scoresZero(){
+    override func setUp() {
+        super.setUp()
         game = startGame(questions: ["Q1", "Q2"], router: router, correctAnswers: ["Q1": "A1", "Q2":"A2"])
-        
+    }
+    
+    func test_startGame_withZeroOfTwoCorrectAnswer_scoresZero(){
         router.answerCallback("wrong")
         router.answerCallback("wrong")
         XCTAssertEqual(router.routedResults?.score, 0)
     }
     
     func test_startGame_withOneOfTwoCorrectAnswer_scoresOne(){
-        game = startGame(questions: ["Q1", "Q2"], router: router, correctAnswers: ["Q1": "A1", "Q2":"A2"])
-        
         router.answerCallback("A1")
         router.answerCallback("wrong")
         XCTAssertEqual(router.routedResults?.score, 1)
     }
     
     func test_startGame_withTwoOfTwoCorrectAnswer_scoresTwo(){
-           game = startGame(questions: ["Q1", "Q2"], router: router, correctAnswers: ["Q1": "A1", "Q2":"A2"])
-           
-           router.answerCallback("A1")
-           router.answerCallback("A2")
-           XCTAssertEqual(router.routedResults?.score, 2)
-       }
+        router.answerCallback("A1")
+        router.answerCallback("A2")
+        XCTAssertEqual(router.routedResults?.score, 2)
+    }
 }
